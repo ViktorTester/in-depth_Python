5ad6a2b77036eb50ea821f9cbfea207d9fa4a949
ViktorTester <viktor345@inbox.lv>
2023-08-19 18:15:40 +0300

    new tasks added

--- file_handling/log_file.py
+++ file_handling/log_file.py
@@ -9,3 +9,28 @@ the name_log.csv file and writes them to the new_name_log.csv file.
 In the new_name_log.csv file, the first line is the column headings,
 the same as in the name_log.csv file. The logs in the final file are
 arranged in the lexicographic order of the names of the user's e-mail boxes."""
+
+import csv
+from datetime import datetime
+
+with open('name_log.csv', encoding='utf-8') as file:
+    rows = list(csv.reader(file))
+
+    d = {}
+    arr = []
+
+    for i in rows[1:]:
+        i[2] = datetime.strptime(i[2], '%d/%m/%Y %H:%M')
+
+    for row in sorted(rows[1:], key=lambda x: x[2]):
+        d[row[1]] = [row[0], row[1], row[2]]
+
+    columns = rows[0]
+
+    for key, value in sorted(d.items(), key=lambda x: x[0]):
+        arr.append({'username': value[0], 'email': key, 'dtime': value[2].strftime('%d/%m/%Y %H:%M')})
+
+with open('new_name_log.csv', 'w', encoding='utf-8', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=columns, delimiter=',')
+    writer.writeheader()
+    writer.writerows(arr)
